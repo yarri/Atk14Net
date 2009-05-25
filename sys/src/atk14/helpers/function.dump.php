e931962523d031060eedbb9f337bdf446cf0b257
yarri <jaromir.tomek@gmail.com>
2009-05-25 21:26:35 +0000

    Fixing loading configuration (Atk14Utils::LoadConfig()) Speeding up generatig URLs (Atk14Url::BuildLink()) Improving helper dump - now it show NULL text if given var is not set

--- sys/src/atk14/helpers/function.dump.php
+++ sys/src/atk14/helpers/function.dump.php
@@ -3,7 +3,7 @@
 * {dump var=$basket->getTotalPrice()}
 */
 function smarty_function_dump($params,&$smarty){
-	$out = print_r($params["var"],true);
+	$out = isset($params["var"]) ? print_r($params["var"],true) : "NULL";
 	return "<pre>".strtr($out,array("<" => "&lt;",">" => "&gt;"))."</pre>";
 }
 ?>
