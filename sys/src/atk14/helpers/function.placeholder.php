80f12e0fd7e29d327dbfb15c2bb5a3850268e2e6
yarri <jaromir.tomek@gmail.com>
2009-07-24 09:53:17 +0000

    adjustment of handling {plachoder} and {content for} helpers now the content for a specific placeholder migh be given after the {placeholder} occurs

--- sys/src/atk14/helpers/function.placeholder.php
+++ sys/src/atk14/helpers/function.placeholder.php
@@ -6,20 +6,13 @@
 *		{placeholder for="head"}
 * </head>
 *	<body>
-* 	{placeholder for="main"}
+* 	{placeholder} {* stands for {placeholder for="main"} *}
 *	</body>
 *	</html>
 */
 function smarty_function_placeholder($params,&$smarty){
-	if(!isset($params["for"])){ $params["for"] = "main"; }
-	$id = $params["for"];
-
-	if(!isset($smarty->atk14_contents) || !isset($smarty->atk14_contents[$id])){
-		//echo "<h1>no content for $id</h1>";
-		return "";
-	}
-	//echo "<h1>content found for $id (".strlen($smarty->atk14_contents[$id])." chars)</h1>";
-
-	return $smarty->atk14_contents[$id];
+	$id = isset($params["for"]) ? $params["for"] : "main"; 
+	if(!isset($smarty->atk14_contents[$id])){ $smarty->atk14_contents[$id] = ""; }
+	return "<%atk14_content[$id]%>"; // returns an internal sign, which will be replaced later within controller
 }
 ?>
