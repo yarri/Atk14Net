6ff60a2b8ca275239ceaf1d0b808a477e37d9ca5
yarri <jaromir.tomek@gmail.com>
2009-12-14 12:57:02 +0000

    new versions of some classes new modifier helper no_spam

--- sys/src/atk14/helpers/block.no_spam.php
+++ sys/src/atk14/helpers/block.no_spam.php
@@ -5,6 +5,10 @@
 * 
 */
 function smarty_block_no_spam($params, $content, &$smarty, &$repeat){
+	return __no_spam_filter__($content);
+}
+
+function __no_spam_filter__($content){
 	return preg_replace("/([^\\s]+)@([^\\s]+)\\.([a-z]{2,5})/i","<span class=\"atk14_no_spam\">\\1[at-sign]\\2[dot-sign]\\3</span>",$content);
 }
 ?>
