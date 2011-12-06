24bda9728cb8246d743a79c654128658eee0cbd0
Jaromir Tomek <jaromir.tomek@gmail.com>
2011-12-06 13:02:00 +0100

    coding for the book

--- app/forms/fields/boolean_field_form.php
+++ app/forms/fields/boolean_field_form.php
@@ -1,10 +1,12 @@
 <?php
-class BooleanFieldForm extends ApplicationForm{
+class BooleanFieldForm extends FieldsForm{
 	function set_up(){
 		$this->set_method("get");
+
 		$this->add_field("bool",new BooleanField(array(
 			"required" => false,
 		)));
+
 		$this->add_field("another_bool",new BooleanField(array(
 			"required" => false,
 			"widget" => new Select(array(
--- app/forms/fields/char_field_form.php
+++ app/forms/fields/char_field_form.php
@@ -7,9 +7,9 @@ class CharFieldForm extends FieldsForm{
 			"label" => "A character value",
 			"help_text" => "Write here a character value",
 			"hint" => "Lorem Ipsum",
-
 			"min_length" => 2, // def. null
 			"max_length" => 20, // def. null
+
 			// "widget" => new TextInput(),
 			// "initial" => "",
 			// "null_empty_output" => false,
