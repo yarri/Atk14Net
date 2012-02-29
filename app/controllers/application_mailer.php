cbc1fd3e87f2e4e8ea358f7ae01f69abbf8bcda1
Jaromir Tomek <jaromir.tomek@gmail.com>
2012-03-01 00:16:09 +0100

    tuning examples

--- app/controllers/application_mailer.php
+++ app/controllers/application_mailer.php
@@ -4,6 +4,11 @@ class ApplicationMailer extends Atk14Mailer {
 		$this->from = "info@atk14.net";
 		$this->to = $params["email_to"];
 		$this->subject = "Book return reminder";
+		// $this->cc = "";
+		// $this->bcc = "";
+		// $this->content_type = "text/plain";
+		// $this->content_charset = "UTF-8";
+
 		$this->tpl_data["book"] = $params["book"];
 	}
 }
--- app/controllers/reminders_controller.php
+++ app/controllers/reminders_controller.php
@@ -1,18 +1,26 @@
 <?php
 class RemindersController extends ApplicationController {
 	function create_new(){
-		$this->doc_source_files[] = "app/controllers/application_mailer.php";
-		$this->doc_source_files[] = "app/views/mailer/send_reminder.tpl";
-
 		$this->page_title = "Book returning reminder";
 
 
 		if($this->request->post() && ($d = $this->form->validate($this->params))){
 			$email_src = $this->mailer->execute("send_reminder",$d);
 
+			// following lines display source code of just sent e-mail...
 			$this->render_template = false;
 			$this->response->setContentType("text/plain");
 			$this->response->write(print_r($email_src,true));
+
+			// more meaningful conclusion may be like
+			// $this->flash->success("The reminder has been sent");
+			// $this->_redirect_to("main/index");
 		}
+	}
+	
+
+	function _before_filter(){
+		$this->doc_source_files[] = "app/controllers/application_mailer.php";
+		$this->doc_source_files[] = "app/views/mailer/send_reminder.tpl";
 	}
 }
