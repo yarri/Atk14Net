f066cd08117ef2165a1517636d514e084e622467
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-05-25 23:10:53 +0200

    [Modernization] ArticlesRouter tuned

--- app/models/article.php
+++ app/models/article.php
@@ -2,14 +2,20 @@
 class Article extends ApplicationModel{
 
 	static function GetInstanceBySlug($slug){
-		if(preg_match('/^(\d+)-.*/',$slug,$matches)){ return Article::FindById($matches[1]); }
+		if(preg_match('/^.*-(\d+)/',$slug,$matches)){ return Article::FindById($matches[1]); }
 	}
 
 	/**
-	 *	echo $article->getSlug(); // "123-best-article-of-the-year"
+	 *	echo $article->getSlug(); // "best-article-of-the-year-1234"
 	 */
 	function getSlug(){
 		$title = new String4($this->getTitle());
-		return $this->getId()."-".$title->toSlug()->truncate(200,["separator" => "-", "omission" => ""])->toString();
+		$slugged_title = $title->toSlug()->truncate(200,["separator" => "-", "omission" => ""])->toString();
+		if(!$slugged_title){
+			// hmm.. I wonder why? :)
+			$slugged_title = "very-nice-article";
+		}
+		$id = $this->getId();
+		return "$slugged_title-$id";
 	}
 }
--- config/routers/articles_router.php
+++ config/routers/articles_router.php
@@ -2,27 +2,37 @@
 /**
  * Router for ArticlesController
  *
- * Provides nice urls for:
+ * Provides nice urls for actions:
  * - articles/index
  * - articles/detail
+ *
+ * Produces routes like:
+ * - /article/
+ * - /articles/very-nice-article-123/
+ * 
+ * czech variants:
+ * - /clanky/
+ * - /clanek/very-nice-article-123/
  */
 class ArticlesRouter extends Atk14Router {
 	
 	function recognize($uri){
-		if(preg_match('/^\/articles\/?$/',$uri)){ // "http://www.atk14.net/articles/"
+		if(preg_match('/^\/(articles|clanky)\/?$/',$uri,$matches)){ // "http://www.atk14.net/articles/"
 			$this->controller = "articles";
 			$this->action = "index";
+			$this->lang = $matches[1]=="articles" ? "en" : "cs";
 			return;
 		}
 
-		if(preg_match('/^\/article\/([0-9a-z-]+)\/?$/',$uri,$matches)){ // "http://www.atk14.net/article/2-atk14-skelet-is-out/"
-			if(!$article = Article::GetInstanceBySlug($matches[1])){
+		if(preg_match('/^\/(article|clanek)\/([0-9a-z-]+)\/?$/',$uri,$matches)){ // "http://www.atk14.net/article/atk14-skelet-is-out-2/"
+			if(!$article = Article::GetInstanceBySlug($matches[2])){
 				$this->_not_found();
 				return;
 			}
 
 			$this->controller = "articles";
 			$this->action = "detail";
+			$this->lang = $matches[1]=="article" ? "en" : "cs";
 
 			$this->params->add("id",$article->getId());
 		}
@@ -32,12 +42,13 @@ class ArticlesRouter extends Atk14Router {
 		if($this->controller!="articles"){ return; }
 
 		if($this->action=="index"){
-			return "/articles/";
+			return $this->lang=="en" ? "/articles/" : "/clanky/";
 		}
 
 		if($this->action=="detail" && ($article = Article::GetInstanceById($this->params->getInt("id")))){
 			$this->params->del("id");
-			return "/article/".$article->getSlug()."/";
+			$prefix = $this->lang=="en" ? "article" : "clanek";
+			return "/$prefix/".$article->getSlug()."/";
 		}
 	}
 }
