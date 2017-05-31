9b761d5c88261d947544168148d37210523b72ea
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-05-31 23:32:17 +0200

    Article tuned

--- app/models/article.php
+++ app/models/article.php
@@ -1,11 +1,21 @@
 <?php
 class Article extends ApplicationModel{
 
+	/**
+	 * Returns an article according to the given URL slug
+	 *
+	 * Returns null when the slug is not correct.
+	 *
+	 *	$article = Article::GetInstanceBySlug("best-article-of-the-year-1234"); // Article
+	 *	$article = Article::GetInstanceBySlug("non-existing-slug"); // null
+	 */
 	static function GetInstanceBySlug($slug){
-		if(preg_match('/^.*-(\d+)/',$slug,$matches)){ return Article::FindById($matches[1]); }
+		if(preg_match('/^.*-(\d+)$/',$slug,$matches)){ return Article::FindById($matches[1]); }
 	}
 
 	/**
+	 * Returns a URL slug of the given article
+	 *
 	 *	echo $article->getSlug(); // "best-article-of-the-year-1234"
 	 */
 	function getSlug(){
--- test/models/tc_article.php
+++ test/models/tc_article.php
@@ -3,12 +3,19 @@ class TcArticle extends TcBase {
 
 	function test(){
 		$a = Article::CreateNewRecord([
+			"id" => 123,
 			"title" => "Sample Article",
 			"teaser" => "Sample Teaser",
 			"body" => "Sample Body",
 			"author" => "John Doe",
 		]);
 
-		$this->assertEquals("sample-article",$a->getSlug());
+		$this->assertEquals("sample-article-123",$a->getSlug());
+
+		$a2 = Article::GetInstanceBySlug("sample-article-123");
+		$this->assertInstanceOf("Article",$a2);
+		$this->assertEquals(123,$a2->getId());
+
+		$this->assertNull(Article::GetInstanceBySlug("non-existing-slug"));
 	}
 }
