dd3ab21c899123ca98f220e6458f7a8ac2245695
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-12-01 20:52:49 +0100

    Documentation

--- app/controllers/api/users_controller.php
+++ app/controllers/api/users_controller.php
@@ -4,13 +4,18 @@ class UsersController extends ApiController {
 	/**
 	 * ### User registration
 	 *
+	 * #### HTTP status codes
+	 *
+	 * * 201 Created
+	 * * 409 Conflict: The login name has already been used
+	 * * 400 Bad Request: There is a invalid parameter
 	 */
 	function create_new(){
 		if($this->request->post() && ($d = $this->form->validate($this->params))){
 
 
 			if(User::FindByLogin($d["login"])){
-				$this->_report_fail("The login has been already taken",409); // HTTP 409 Conflict
+				$this->_report_fail("The login name has already been used",409); // HTTP 409 Conflict
 				return;
 			}
 
