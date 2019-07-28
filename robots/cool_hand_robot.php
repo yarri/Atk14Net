3abbe50d936dfe551035c3a6222e7ca002c73738
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-07-28 12:51:29 +0200

    CoolHandRobot tuned

--- robots/cool_hand_robot.php
+++ robots/cool_hand_robot.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * An example robot which logs a message and then does nothing
+ */
 class CoolHandRobot extends ApplicationRobot{
+
 	function run(){
+		// In here there is access to:
+		//
+		//	$this->logger
+		//	$this->dbmole
+		//	$this->mailer
+		//	...
+
 		$this->logger->info("The Cool Hand robot is eating eggs");
 	}
 }
